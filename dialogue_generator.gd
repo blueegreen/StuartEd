6643e11c597695f1ebfb33db35424d1183744429
blueegreen <100084909+blueegreen@users.noreply.github.com>
2025-07-15 21:05:30 +0530

    .

--- dialogue_generator.gd
+++ dialogue_generator.gd
@@ -5,7 +5,7 @@ class_name test_dialogue_generator
 #script: ["A: hello", "B: hi"] and so on
 #context_data: {A: {"marker":ref, "animatedsprite":ref}, B: {"maker":ref, "animatedsprite":ref}}
 
-var dialogue_box = preload("res://dialogue_box.tscn")
+var dialogue_box_scene = preload("res://dialogue_box.tscn")
 var active_box : Node2D = null
 var current_script : Array[String] = []
 var current_context : Dictionary = {}
@@ -25,10 +25,45 @@ func start_dialogue(script:Array, context_data:Dictionary):
 	is_dialogue_active = true
 
 func _show_next_line():
+	if active_box:
+		active_box.queue_free()
+		active_box = null
+
 	if current_index >= current_script.size():
 		_end_dialogue()
 		return
 	
+	var line = current_script[current_index]
+	current_index += 1
+	var parts = line.split(":", false, 2)
+	if parts.size() != 2:
+		_show_next_line()
+		return
+	
+	var speaker_id = parts[0].strip_edges()
+	var text = parts[1].strip_edges()
+	
+	var speaker_data = current_context.get(speaker_id)
+	if speaker_data == null:
+		_show_next_line()
+		return
+	
+	var marker = speaker_data.marker
+	
+	if speaker_data.animator:
+		if speaker_data.animator.has_animation("talk"):
+			speaker_data.animator.play("talk")
+
+	active_box = dialogue_box_scene.instantiate()
+	active_box.set_text(text)
+	get_tree().current_scene.add_child(active_box)
+	active_box.global_position = marker.global_position
+	
+	await get_tree().create_timer(2.0).timeout
+	if speaker_data.animator:
+		if speaker_data.animator.has_animation("idle"):
+			speaker_data.animator.play("idle")
+
 
 func _end_dialogue():
 	dialogue_finished.emit()
