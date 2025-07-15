6fce81472d9a700850856341f7bc36dc431a4dc4
blueegreen <100084909+blueegreen@users.noreply.github.com>
2025-07-15 22:32:37 +0530

    .

--- dialogue_box.gd
+++ dialogue_box.gd
@@ -1,1 +1,4 @@
 extends RichTextLabel
+
+func set_custom_text(text:String, animation:int = 0):
+	pass
--- dialogue_generator.gd
+++ dialogue_generator.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name test_dialogue_generator
 
 #scripts are to be given in this format: (script, context_data)
-#script: ["A: hello", "B: hi"] and so on
+#script: ["A: hello", "B: hi: 2"] and so on
 #context_data: {A: {"marker":ref, "animatedsprite":ref}, B: {"maker":ref, "animatedsprite":ref}}
 
 var dialogue_box_scene = preload("res://dialogue_box.tscn")
@@ -15,7 +15,7 @@ var is_dialogue_active := false
 signal dialogue_finished
 
 func _input(event):
-	if is_dialogue_active:
+	if is_dialogue_active and event.is_action_pressed("click") and not event.is_echo():
 		_show_next_line()
 		get_viewport().set_input_as_handled()
 
@@ -35,14 +35,17 @@ func _show_next_line():
 	
 	var line = current_script[current_index]
 	current_index += 1
-	var parts = line.split(":", false, 2)
-	if parts.size() != 2:
+	var parts = line.split(":", false, 3)
+	if parts.size() < 2:
 		_show_next_line()
 		return
 	
+	var text_animation = 0
 	var speaker_id = parts[0].strip_edges()
 	var text = parts[1].strip_edges()
-	
+	if parts.size() > 2:
+		text_animation = parts[2].strip_edges().to_int()
+		
 	var speaker_data = current_context.get(speaker_id)
 	if speaker_data == null:
 		_show_next_line()
@@ -55,7 +58,7 @@ func _show_next_line():
 			speaker_data.animator.play("talk")
 
 	active_box = dialogue_box_scene.instantiate()
-	active_box.set_text(text)
+	active_box.set_custom_text(text, text_animation)
 	get_tree().current_scene.add_child(active_box)
 	active_box.global_position = marker.global_position
 	
