7cad29309e90fe34c07453f436050d8fb59f4311
eevx <155942309+eevx@users.noreply.github.com>
2025-07-15 22:33:00 +0530

    changes

--- dialogue_box.gd
+++ dialogue_box.gd
@@ -6,3 +6,7 @@ func _ready() -> void:
 
 func append_chat_line(object, message):
 	append_text("[wave]%s: [color=green]%s[/color][/wave]\n" % [object, message])
+
+func append_monlogue_line(message :String,animation : int):
+	if animation == 0:
+		append_text("[wave]" + message +"[/wave]")
