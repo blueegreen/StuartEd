469dc5b4034811223aef8958b05298e66d28d5f2
eevx <155942309+eevx@users.noreply.github.com>
2025-07-15 22:22:02 +0530

    smth

--- dialogue_box.gd
+++ dialogue_box.gd
@@ -1,1 +1,8 @@
 extends RichTextLabel
+
+func _ready() -> void:
+	self.text =""
+	append_chat_line("player","hello")
+
+func append_chat_line(object, message):
+	append_text("[wave]%s: [color=green]%s[/color][/wave]\n" % [object, message])
