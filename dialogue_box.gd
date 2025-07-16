04beff4c81c343da2a24a4e7d107f7a021583ec0
eevx <155942309+eevx@users.noreply.github.com>
2025-07-16 09:12:49 +0530

    kaam

--- dialogue_box.gd
+++ dialogue_box.gd
@@ -1,4 +1,27 @@
 extends RichTextLabel
+class_name dialogue_box
+@export var dict : Dictionary = {"Message":"@","Animation":0}
 
-func set_custom_text(text:String, animation:int = 0):
-	pass
+func _ready() -> void:
+	self.text =""
+	append_monlogue_line(dict["Message"],dict["Animation"])
+
+func append_chat_line(object, message):
+	append_text("[wave]%s: [color=green]%s[/color][/wave]\n" % [object, message])
+
+func append_monlogue_line(message :String,animation : int = 0):
+	match animation:
+		0:
+			append_text(message)
+		1:
+			append_text("[pulse]"+ message +"[/pulse]")
+		2:
+			append_text("[wave]"+ message +"[/wave]")
+		3:
+			append_text("[tornado]"+ message +"[/tornado]")
+		4:
+			append_text("[shake]"+ message +"[/shake]")
+		5:
+			append_text("[rainbow]"+ message +"[/rainbow]")
+		_:
+			append_text(message)
